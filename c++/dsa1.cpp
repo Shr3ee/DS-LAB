5b63b66e88c2f5bc07d4bb233c52039dd29aafa3
Rajshree <rajshreebhuyan1306@gmail.com>
2026-07-28 15:51:21 +0530

    brainstorming

---
+++ c++/dsa1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,c=0;
+    // scanf("%d%d",&a,&b);
+    // while(a!=b){
+    //     if(a>b) b=b+2;
+    //     else    a++;
+    //     c++;}
+    // printf("%d",c);
+    cin>>a>>b;
+    if(a==b)    cout<<c;
+    else if (a<b)   b-a;
+    else{
+        int diff=a-b;
+        if(diff%2==0)   c=diff/2;
+        else    c=(a-b)/2+2;
+    }
+    cout<<c;
+    
+}
